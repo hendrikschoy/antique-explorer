2fa850482f9a5199c3fd569f6409b43c681d6cfa
Hendrik Choy <hendrik.s.choy@gmail.com>
2022-04-25 20:23:06 -0400

    add dependecnies

---
+++ src/Catalog.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const Catalog = (props) => {
+
+    useEffect(() => {
+        // fetch catalog items
+    }, [])
+
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Catalog;
